d8faeb153c80497a7099e4570e06ea5eacac1efc
Tiago Royer <royertiago@gmail.com>
2015-05-24 17:41:47 -0300

    main.cpp: Reseed every time.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
 #include "kohonen_pallete.h"
+#include "std_rand_wrapper.h"
 
 void draw_network( const KohonenPallete & knn, cv::Mat& img ) {
     for( int i = 0; i < img.rows; i++ )
@@ -12,21 +13,29 @@ void draw_network( const KohonenPallete & knn, cv::Mat& img ) {
 }
 
 int main() {
+    std_rand_wrapper::time_seed();
+    std::cout << "Seed: " << std_rand_wrapper::seed() << '\n';
     std::vector< color > data{
         {255,   0,   0},
         {  0, 255,   0},
         {  0,   0, 255},
+        {255, 255,   0},
+        {  0, 255, 255},
+        {255,   0, 255},
+        {255, 255, 255},
+        {  0,   0,   0},
     };
 
-    KohonenPallete knn( 30, 40, data ); // Kohonen Neural Network
+    int rows = 30, cols = 40;
+    KohonenPallete knn( rows, cols, data ); // Kohonen Neural Network
 
     cv::namedWindow( "Kohonen", cv::WINDOW_NORMAL);
-    cv::Mat img( 30, 40, CV_8UC3 );
+    cv::Mat img( rows, cols, CV_8UC3 );
 
-    for( int i = 0; i < 1000; i++ ) {
+    for( int i = 0; i < 100; i++ ) {
         draw_network( knn, img );
         cv::imshow( "Kohonen", img );
-        cv::waitKey( 0 );
+        cv::waitKey( 1 );
 
         std::cout << "Training " << i << '\n';
         knn.train();
